b1e464fb1bcfcc90e876cec24fcd80b6d79260c5
J. Logan Affleck <affleck.logan@gmail.com>
2021-06-26 00:23:24 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ const errorHandler = require("./errors/errorHandler")
 const notFound = require('./errors/notFound');
 const moviesRouter = require('./movies/movies.router');
 const theatersRouter = require('./theaters/theaters.router')
+const reviewsRouter = require('./reviews/reviews.router')
 //
 
 //Use JSON format for data return.
@@ -22,6 +23,7 @@ app.use(cors());
 //Direct request based on URL
 app.use("/movies", moviesRouter);
 app.use("/theaters", theatersRouter);
+app.use("/reviews", reviewsRouter);
 
 //Generate error "Not Found" for invalid URL path
 app.use(notFound);
